9415ce71154cb1bcb73e2d2f52e50d3fc62e6774
Mateus Fernandes <mateusveloso2608@gmail.com>
2025-01-11 14:47:01 -0300

    Corrigindo o componente footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,11 +1,17 @@
-import { FaInstagram, FaFacebook } from "react-icons/fa";
-import { RiTwitterXFill } from "react-icons/ri";
+import { FaInstagram, FaFacebook, FaLinkedin } from "react-icons/fa";
 
 export function Footer() {
   return (
     <footer className="bg-gray-900 text-white py-8">
       <div className="container mx-auto px-6">
         <div className="flex justify-around max-md:flex-col max-md:items-center">
+
+        <div className="text-center my-4">
+            <h3 className="text-xl font-semibold mb-4">Contato</h3>
+            <p className="text-sm">Email: ProLar@gmail.com</p>
+            <p className="text-sm">Telefone: (71) xxxx-xxxx</p>
+          </div>
+
           <div className="text-center my-4">
             <h3 className="text-xl font-semibold mb-4">ProLar</h3>
             <p className="text-sm max-w-sm">
@@ -14,18 +20,21 @@ export function Footer() {
             </p>
           </div>
 
-          <div className="text-center my-4">
-            <h3 className="text-xl font-semibold mb-4">Contato</h3>
-            <p className="text-sm">Email: ProLar@gmail.com</p>
-            <p className="text-sm">Telefone: (11) 98765-4321</p>
-          </div>
-
           <div className="flex flex-col text-center my-4 ">
             <h3 className="text-xl font-semibold mb-4">Redes sociais</h3>
             <div className="flex gap-3 justify-center">
-              <FaInstagram size={24} />
-              <FaFacebook size={24} />
-              <RiTwitterXFill size={24} />
+              <FaInstagram
+                size={24}
+                className="hover:text-blue-500 transition-all duration-200 cursor-pointer"
+              />
+              <FaFacebook
+                size={24}
+                className="hover:text-blue-500 transition-all duration-200 cursor-pointer"
+              />
+              <FaLinkedin
+                size={24}
+                className="hover:text-blue-500 transition-all duration-200 cursor-pointer"
+              />
             </div>
           </div>
         </div>
